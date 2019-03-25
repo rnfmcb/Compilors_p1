a02362ec703310e22060a0e24f52219edb4ddf58
Festervand <rnfmcb@delmar.umsl.edu>
2019-03-25 10:28:32 -0500

    Added delimiters

--- scanner.cpp
+++ scanner.cpp
@@ -24,11 +24,22 @@ void Scanner::makeToken(string type, int line){
 void Scanner:: checkType (char x, int line) { 
 	string type; 
     line = 0; 
-
-	if(x == Num[0]) { 
-		type = "numId"; 
+    //If char is a number, then it is a int
+	if(x == Num[0] | x == Num[1] | x == Num[2] | x == Num[3] | x == Num[4] | x == Num[5] | x == Num[6] | x == Num[7] | x == Num[8] | x == Num[9])  { 
+		type = "intId"; 
     	makeToken(type, line);  
 	}
+    // =  <  >  :   +  -  *  /   %  . (  ) , { } ; [ ]
+    else if ( x == delimiter[0] |  x == delimiter[1] | x == delimiter[2] | x == delimiter[3] |  x == delimiter[4] | x == delimiter[5] | x == delimiter[6]
+	| x == delimiter[7] | x == delimiter[8] | x == delimiter[9] | x == delimiter[10] | x == delimiter[11] | x == delimiter[12] | x == delimiter[13] |
+	 x == delimiter[14] | x == delimiter[15] | x == delimiter[16]){
+   	    type = "delId"; 
+		makeToken(type,line); 
+    }  	  	
+
+
+
+
 } 
 
 
--- token.h
+++ token.h
@@ -12,6 +12,7 @@ using namespace std;
 const char Num[]  = {'1','2','3','4','5','6','7','8','9','0'};
 const char letters[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 const char capitals[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};  
+const char delimiter[] = {'=','<','>',':','+','-','*','/','%','.',',',';','(',')','{','}','[',']'}; 
 const string tokens[] = {"INTTkn","ltrTkn","delTkn","begintkn","endTkn","LoopTkn","voidTkn", "returnTkn","readTkn","outputTkn","programTkn","IFFTkn","thenTkn","LetTkn","wsTkn","charTkn","eofTkn"};
 const string keywords[] = {"Begin","End","Loop","Void","INT","Return","Read","Output","Program","IFF","Then","Let"};
 
